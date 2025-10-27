300231388f21dddcb6a0d1b4dbc7505a88091643
Kapukawi <kapukawi600@gmail.com>
2025-10-28 05:55:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  BOT_TOKEN: "7915979146:AAE2ZWk6PKI9XILZs8Oyery9wiH0SxVf83I",//bot tokenlu
+  OWNER_ID: ["7584587633"],//id lu
+  API_HASH: "01affc9ea4e8a587d39b640ac19a5cf9",
+  API_ID: 21290664,//ganti 0 dan langsung isi angka nya saja ex: 12323232
+  //Baca Readme.txt agar kalian faham cara menggunakan ban CH ( Login Dengan Akun Telegram Premium Lebih Work Dan Cepat Terban ) terima kasih
+  // Thanks To Ota And Dimz
+};
+//BY OTAX
